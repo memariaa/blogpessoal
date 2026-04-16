6b2bfa897670d8dfbd9597db35a296580e84c632
Maria Eduarda <mariaeduardao.gms@gmail.com>
2026-04-15 22:43:16 -0300

    feat: implementa crud perfil

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,30 +1,46 @@
-import { GithubLogoIcon, LinkedinLogoIcon } from "@phosphor-icons/react"
+import { FacebookLogoIcon, InstagramLogoIcon, LinkedinLogoIcon } from "@phosphor-icons/react"
+import { useContext, type ReactNode } from "react"
+import { AuthContext } from "../../contexts/AuthContext"
 
 function Footer() {
 
-    let data = new Date().getFullYear()
+  let data = new Date().getFullYear()
+
+  const { usuario } = useContext(AuthContext)
+
+  let component: ReactNode
+
+  if (usuario.token !== "") {
+
+    component = (
+
+      <div className="flex justify-center bg-indigo-900 text-white">
+        <div className="container flex flex-col items-center py-4">
+          <p className='text-xl font-bold'>
+            Blog Pessoal Generation | Copyright: {data}
+          </p>
+          <p className='text-lg'>Acesse nossas redes sociais</p>
+          <div className='flex gap-2'>
+            <a href="https://www.linkedin.com/in/seu_usuario" target="_blank">
+              <LinkedinLogoIcon size={48} weight='bold' />
+            </a>
+            <a href="https://www.instagram.com/seu_usuario" target="_blank">
+              <InstagramLogoIcon size={48} weight='bold' />
+            </a>
+            <a href="https://www.facebook.com/seu_usuario" target="_blank">
+              <FacebookLogoIcon size={48} weight='bold' />
+            </a>
+          </div>
+        </div>
+      </div>
+
+    )
+  }
 
   return (
-    <div className='flex justify-center bg-indigo-900 text-white'>
-        <div className='container flex flex-col items-center py-4'>
-            <p className='text-xl font-bold'>
-                © {data} | Copyright: Blog Pessoal - memaria
-            </p>
-
-            <p className='text-lg'>
-                Acesse minhas redes sociais:
-            </p>
-
-            <div className='flex gap-2'>
-                <a href='https://linkedin.com/in/memaria' target="_blank" rel="noopener noreferrer">
-                    <LinkedinLogoIcon size={32} weight="bold" />
-                </a>
-                <a href='https://github.com/memaria' target="_blank" rel="noopener noreferrer">
-                    <GithubLogoIcon size={32} weight="bold" />
-                </a>
-            </div>
-        </div>
-    </div>
+    <>
+      { component }
+    </>
   )
 }
 
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,31 +1,48 @@
-import { useContext } from "react";
-import { Link, useNavigate } from "react-router-dom"
+import { useContext, type ReactNode } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../contexts/AuthContext";
 
-function Header() {
+function Navbar() {
+
   const navigate = useNavigate();
-  const { handleLogout } = useContext(AuthContext)
+
+  const { usuario, handleLogout } = useContext(AuthContext)
 
   function logout() {
     handleLogout()
-    alert("Usuário deslogado com sucesso!")
+    alert('O usuário foi desconectado com sucesso!')
     navigate('/')
   }
-  
-  return (
-    <div className='w-full flex justify-center py-4 bg-indigo-900 text-white'>
-        <div className='container flex justify-between text-lg mx-8'>
-            <Link to='/' className='text-2xl font-bold'>Blog Pessoal</Link>
-            <div className='flex gap-4'>
-                <Link to='/postagens' className='hover:underline'>Postagens</Link>
-                <Link to='/temas' className='hover:underline'>Temas</Link>
-                <Link to='/cadastrartema' className='hover:underline'>Cadastrar Tema</Link>
-                Perfil
-                <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
-            </div>
+
+  let component: ReactNode
+
+  if (usuario.token !== "") {
+
+    component = (
+
+      <div className='w-full flex justify-center py-4 bg-indigo-900 text-white'>
+
+        <div className="container flex justify-between text-lg mx-8">
+          <Link to='/home' className="text-2xl font-bold">Blog Pessoal</Link>
+
+          <div className='flex gap-4'>
+            <Link to='/postagens' className='hover:underline'>Postagens</Link>
+            <Link to='/temas' className='hover:underline'>Temas</Link>
+            <Link to='/cadastrartema' className='hover:underline'>Cadastrar tema</Link>
+            <Link to='/perfil' className='hover:underline'>Perfil</Link>
+            <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
+          </div>
         </div>
-    </div>
+      </div>
+
+    )
+  }
+
+  return (
+    <>
+      { component }
+    </>
   )
 }
 
-export default Header
+export default Navbar
